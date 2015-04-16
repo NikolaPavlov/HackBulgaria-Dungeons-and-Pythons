2007aac98315ab3b40d6e6737b3eb0b1b0761089
presianbg <presianbg@gmail.com>
2015-04-16 21:29:33 +0300

    Adding some Basic Movements

--- Dungeons.py
+++ Dungeons.py
@@ -1,8 +1,12 @@
+from Hero import Hero
+
+
 class Dungeon:
     def __init__(self, level="level1.txt"):
         self.level = level
         self.dungeon_map = []
         self.load_level()
+        self.hero_current_position = None
 
     def load_level(self):
         with open(self.level) as f:
@@ -10,5 +14,65 @@ class Dungeon:
                 self.dungeon_map.append(line.strip().split(','))
 
     def show_map(self):
-        for map_proc in self.dungeon_map:
-            print(''.join(map_proc))
+        for map_path in self.dungeon_map:
+            print(''.join(map_path))
+
+    def spawn(self, hero_to_spawn):
+
+        if type(hero_to_spawn) is not Hero:
+            raise ThisIsNotAHero
+
+        for path_num, path in enumerate(self.dungeon_map):
+            for step_num, step in enumerate(path):
+                if step == 'S':
+                    self.dungeon_map[path_num][step_num] = 'H'
+                    self.hero_current_position = (path_num, step_num)
+                    return True
+        return False
+
+    def move_hero(self, direction):
+        possible_directions = set(["up", "down", "left", "right"])
+
+        if direction not in possible_directions:
+            raise WrongDirection
+
+        if direction == "up":
+            return self.move_up()
+
+        elif direction == "left":
+            return self.move_left()
+
+        elif direction == "right":
+            return self.move_right()
+
+        elif direction == "down":
+            return self.move_down()
+
+    def move_up(self):
+        if self.hero_current_position[0] - 1 < 0:
+            return False
+        return True
+
+    def move_down(self):
+        if self.hero_current_position[0] + 1 > len(self.dungeon_map):
+            return False
+        return True
+
+    def move_right(self):
+        num_path = self.hero_current_position[0]
+        if self.hero_current_position[1] + 1 > len(self.dungeon_map[num_path]):
+            return False
+        return True
+
+    def move_left(self):
+        if self.hero_current_position[1] - 1 < 0:
+            return False
+        return True
+
+
+
+class ThisIsNotAHero(Exception):
+    pass
+
+class WrongDirection(Exception):
+    pass
--- test_Dungeons.py
+++ test_Dungeons.py
@@ -1,17 +1,37 @@
 import unittest
-from Dungeons import Dungeon
+from Dungeons import Dungeon, ThisIsNotAHero, WrongDirection
+from Hero import Hero
 
 
 class TestDungeons(unittest.TestCase):
 
     def setUp(self):
         self.outcast_land = Dungeon()
+        self.fighter = Hero()
+        self.outcast_land.spawn(self.fighter)
 
     def test_loading_map(self):
         self.assertEqual(type(self.outcast_land.dungeon_map), list)
 
     def test_show_map(self):
+        print ("===== BEFORE ======")
         self.outcast_land.show_map()
+
+    def test_spawn(self):
+        alabala = "WTF"
+        with self.assertRaises(ThisIsNotAHero):
+            self.outcast_land.spawn(alabala)
+        print("==== AFTER =====")
+        self.outcast_land.show_map()
+
+    def test_move_hero(self):
+        with self.assertRaises(WrongDirection):
+            self.outcast_land.move_hero("asdad")
+        self.outcast_land.spawn(self.fighter)
+        self.assertFalse(self.outcast_land.move_hero("up"), False)
+        self.assertFalse(self.outcast_land.move_hero("left"), False)
+        self.assertTrue(self.outcast_land.move_hero("down"), True)
+        self.assertTrue(self.outcast_land.move_hero("right"), True)
 
 
 if __name__ == "__main__":
