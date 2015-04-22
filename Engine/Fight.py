aa358198f22f211bf773ae5efd51983fc235e198
presianbg <presianbg@gmail.com>
2015-04-22 10:54:10 +0300

    Better Fight() still in beta

--- Engine/Fight.py
+++ Engine/Fight.py
@@ -9,7 +9,8 @@ class Fight:
         self.enemy_pos = enemy_pos
         self.fight_type = fight_type
         self.enemy = Enemy(100, 100, 30)
-        self.hero_direct = None
+        self.distfight = True
+        self.hero_drect_dist = self.find_direct_and_dist()
 
     def find_direct_and_dist(self):
         new_pos = tuple([ny - nx for ny, nx in zip(self.hero_pos, self.enemy_pos)])
@@ -23,8 +24,34 @@ class Fight:
                 return ['up', abs(new_pos[0])]
             return ['down', abs(new_pos[0])]
 
+    def hero_fight_from_distance(self):
+        if (self.fight_type == 'spell' and self.hero.can_cast()) or self.fight_type == 'weapon':
+            dmg_done = self.hero.attack(by=self.fight_type)
+            self.enemy.take_damage(dmg_done)
+            msg = 'Hero hit the enemy with {}-{}for {} dmg'.format(self.fight_type, self.hero.has_spell['name'], dmg_done)
+            return msg
+        msg = 'Hero have no mana to cast the spell or weapon has not enough range to atack the enemy. Standing still.'
+        return msg
+
+    def hero_close_fight(self):
+        if self.is_spell_more_eq_dmg() and self.hero.can_cast():
+            self.enemy.take_damage(self.hero.attack(by='spell'))
+        self.enemy.take_damage(self.hero.attack(by='weapon'))
+
+    def is_spell_more_eq_dmg(self):
+        return self.hero.attack(by='spell') >= self.hero.attack(by='weapon')
+
+    def enemy_move(self):
+        if self.hero_drect_dist[1] > 1:
+            self.hero_drect_dist[1] -= 1
+            print('Enemy moves one step to the {} in order to get to the hero. This is his move.'.format(self.hero_drect_dist[0]))
+            return True
+        return False
+
     def fight_scenario(self):
-        if self.fight_type == 'spell' or self.fight_type == 'weapon':
-            if self.hero.can_cast():
-                self.enemy.take_damage(self.hero.attack(by=self.fight_type))
+        while self.distfight:
+            if self.hero_drect_dist[1] >= 2:
+                print(self.hero_fight_from_distance())
+            self.distfight = self.enemy_move()
+
 
--- tests/test_Fight.py
+++ tests/test_Fight.py
@@ -4,21 +4,30 @@ sys.path.insert(0, '../Engine')
 sys.path.insert(0, '../resources')
 from Fight import Fight
 from Hero import Hero
+from Spell import Spell
+from Weapon import Weapon
 
 
 class TestFight(unittest.TestCase):
 
     def setUp(self):
         self.pudge = Hero()
-        self.battle = Fight(self.pudge, (4, 6), (4, 5), 'spell')
+        self.magic = Spell(mana_cost=1)
+        self.sword = Weapon(damage=60)
+        self.pudge.learn(self.magic)
+        self.pudge.equip(self.sword)
+        self.battle = Fight(self.pudge, (4, 8), (4, 5), 'spell')
 
-    def test_determinate_hero_direct(self):
+    def test_direct_and_dist(self):
         direct_and_dist = self.battle.find_direct_and_dist()
         self.assertEqual(direct_and_dist[0], 'right')
-        self.assertEqual(direct_and_dist[1], 1)
+        self.assertEqual(direct_and_dist[1], 3)
+
+    def test_is_spell_more_eq_dmg(self):
+        self.assertFalse(self.battle.is_spell_more_eq_dmg())
 
     def test_fight_scenario(self):
-        pass
+        self.battle.fight_scenario()
 
 
 if __name__ == '__main__':
